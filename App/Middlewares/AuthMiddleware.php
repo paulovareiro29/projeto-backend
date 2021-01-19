b01bf60a20a1f826748f6f3022dea2a5ff97b6f6
BuildTools <unconfigured@null.spigotmc.org>
2021-01-19 17:01:22 +0000

    Added auth

--- App/Middlewares/AuthMiddleware.php
+++ App/Middlewares/AuthMiddleware.php
@@ -2,33 +2,99 @@
 
 namespace App\Middlewares;
 
+use App\DAO\Database\UserDAO;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
 
-class AuthMiddleware {
+class AuthMiddleware
+{
 
-    public function isAdmin(Request $request, Response $response, $next){
-        //POR FAZER
-       $response = $next($request,$response);
-      
-        return $response;
+    public function isAdmin(Request $request, Response $response, $next)
+    {
+        $userDAO = new UserDAO();
+
+        $token = $request->getHeader('TOKEN');
+
+        $user = $userDAO->getUserByToken($token);
+
+        if (!isset($user)) { //verificar se o utilizador existe associado ao respetivo token
+            $response->getBody()->write(json_encode("Unauthenticated"), JSON_UNESCAPED_UNICODE);
+            return $response
+                ->withHeader('Content-Type', 'application/json')
+                ->withStatus(401);
+        }
+
+        if ($userDAO->isAdmin($user['id'])) {
+            $response = $next($request, $response);
+            return $response;
+        }
+
+        $response->getBody()->write(json_encode("Unauthorized"), JSON_UNESCAPED_UNICODE);
+        return $response
+            ->withHeader('Content-Type', 'application/json')
+            ->withStatus(403);
     }
 
-    public function isAtleta(Request $request, Response $response, $next){
-        //POR FAZER
-        $response = $next($request,$response);
-        
-        return $response;
+    public function isAtleta(Request $request, Response $response, $next)
+    {
+        $userDAO = new UserDAO();
+
+        $token = $request->getHeader('TOKEN');
+
+        $user = $userDAO->getUserByToken($token);
+
+        if (!isset($user)) { //verificar se o utilizador existe associado ao respetivo token
+            $response->getBody()->write(json_encode("Unauthenticated"), JSON_UNESCAPED_UNICODE);
+            return $response
+                ->withHeader('Content-Type', 'application/json')
+                ->withStatus(401);
+        }
+
+        if ($userDAO->isAtleta($user['id'])) {
+            $response = $next($request, $response);
+            return $response;
+        }
+
+        $response->getBody()->write(json_encode("Unauthorized"), JSON_UNESCAPED_UNICODE);
+        return $response
+            ->withHeader('Content-Type', 'application/json')
+            ->withStatus(403);
     }
 
-    public function isTreinador(Request $request, Response $response, $next){
-        //POR FAZER
-        
-        $response = $next($request,$response);
-        
-        return $response;
+    public function isTreinador(Request $request, Response $response, $next)
+    {
+        $userDAO = new UserDAO();
+
+        $token = $request->getHeader('TOKEN');
+
+        $user = $userDAO->getUserByToken($token);
+
+        if (!isset($user)) { //verificar se o utilizador existe associado ao respetivo token
+            $response->getBody()->write(json_encode("Unauthenticated"), JSON_UNESCAPED_UNICODE);
+            return $response
+                ->withHeader('Content-Type', 'application/json')
+                ->withStatus(401);
+        }
+
+        if ($userDAO->isTreinador($user['id']) || $userDAO->isAdmin($user['id'])) {
+            $response = $next($request, $response);
+            return $response;
+        }
+
+        $response->getBody()->write(json_encode("Unauthorized"), JSON_UNESCAPED_UNICODE);
+        return $response
+            ->withHeader('Content-Type', 'application/json')
+            ->withStatus(403);
     }
 
 
+    public function headers(Request $request, Response $response, $next)
+    {
+        $response = $next($request, $response);
+        return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, token, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    }
 }
--- routes/index.php
+++ routes/index.php
@@ -10,85 +10,90 @@ use function src\slimConfiguration;
 
 $app = new \Slim\App(slimConfiguration());
 
-// --------------------------------------
-$app->post('/login', UserController::class . ':login');
+$app->add(AuthMiddleware::class . ':headers');
+//------------------------------------
+    $app->post('/login', UserController::class . ':login');
 
-$app->put('/plano/blocoexercicio/{bloco}/{exercicio}', PlanoController::class . ':updateExercise');
+    $app->put('/plano/blocoexercicio/{bloco}/{exercicio}', PlanoController::class . ':updateExercise');
 
-$app->group('/user', function() use ($app){
-    $app->get('/', UserController::class . ':index');
-    $app->get('/{id}', UserController::class . ':show');
-    $app->put('/{id}', UserController::class . ':update');
-    $app->get('/token/{token}', UserController::class . ':getByToken');
-    $app->put('/token/{token}', UserController::class . ':updateByToken');
-    $app->group('', function() use ($app){
-        $app->post('/', UserController::class . ':create');
-    
-        $app->delete('/{id}', UserController::class . ':delete');
-    
-        $app->post('/associate', UserController::class . ':associate');
-        $app->post('/dissociate', UserController::class . ':dissociate');
-    })->add(AuthMiddleware::class . ':isAdmin'); //somente admins podem fazer isto
-});
+    $app->group('/user', function () use ($app) {
 
-//plano
-$app->group('/plano', function() use($app){
+        $app->get('/{id}', UserController::class . ':show');
+        $app->put('/{id}', UserController::class . ':update');
+        $app->get('/token/{token}', UserController::class . ':getByToken');
+        $app->put('/token/{token}', UserController::class . ':updateByToken');
+        $app->group('', function () use ($app) {
+            $app->get('/', UserController::class . ':index');
 
-    $app->get('/{id}/bloco/{dia}', PlanoController::class . ':showBloco');//
+            $app->post('/', UserController::class . ':create');
 
-    //atletas/admins
-    $app->group('', function () use ($app){
-        $app->get('/atleta/{id}', PlanoController::class . ':planosAtleta'); //
+            $app->delete('/{id}', UserController::class . ':delete');
 
-    })->add(AuthMiddleware::class . ':isAtleta');
+            $app->post('/associate', UserController::class . ':associate');
+            $app->post('/dissociate', UserController::class . ':dissociate');
+        })->add(AuthMiddleware::class . ':isAdmin'); //somente admins podem fazer isto
+    });
 
+    //plano
+    $app->group('/plano', function () use ($app) {
 
-    //treinadores/admins
-    $app->group('', function() use ($app){
-        $app->get('/{id}', PlanoController::class . ':show');//
-        $app->get('/treinador/{id}', PlanoController::class . ':planosTreinador');//
-        $app->get('/{id}/atletas', PlanoController::class . ':atletasAssociadosPlano');//
-        $app->post('/', PlanoController::class . ':create');//
-        $app->post('/associate', PlanoController::class . ':associate');//
-        $app->post('/dissociate', PlanoController::class . ':dissociate');//
-        $app->post('/{id}/add', PlanoController::class . ':addExercise');//
-        $app->post('/duplicate', PlanoController::class . ':duplicate');
+        $app->get('/{id}/bloco/{dia}', PlanoController::class . ':showBloco'); //
 
-        $app->put('/{id}', PlanoController::class . ':update');//
-        $app->delete('/{id}', PlanoController::class . ':delete');//
-        $app->delete('/blocoexercicio/{bloco}/{exercicio}', PlanoController::class . ':deleteExercise');//
-    })->add(AuthMiddleware::class . ':isTreinador');
+        //atletas/admins
+        $app->group('', function () use ($app) {
+            $app->get('/atleta/{id}', PlanoController::class . ':planosAtleta'); //
+
+        })->add(AuthMiddleware::class . ':isAtleta');
 
 
-    //admins
-    $app->group('',function() use($app){
-        $app->get('/', PlanoController::class . ':index');//
+        //treinadores/admins
+        $app->group('', function () use ($app) {
+            $app->get('/{id}', PlanoController::class . ':show'); //
+            $app->get('/treinador/{id}', PlanoController::class . ':planosTreinador'); //
+            $app->get('/{id}/atletas', PlanoController::class . ':atletasAssociadosPlano'); //
+            $app->post('/', PlanoController::class . ':create'); //
+            $app->post('/associate', PlanoController::class . ':associate'); //
+            $app->post('/dissociate', PlanoController::class . ':dissociate'); //
+            $app->post('/{id}/add', PlanoController::class . ':addExercise'); //
+            $app->post('/duplicate', PlanoController::class . ':duplicate');
 
-    })->add(AuthMiddleware::class . ':isAdmin');
-});
-
-//exercicio
-$app->group('/exercicio', function() use ($app){
-
-    //treinador
-    $app->group('', function() use ($app){
-        $app->get('/', ExercicioController::class . ':index');
-        $app->get('/{id}', ExercicioController::class . ':show');
-        $app->post('/', ExercicioController::class . ':create');
-        $app->put('/{id}', ExercicioController::class . ':update');
-        $app->delete('/{id}',  ExercicioController::class . ':delete');
+            $app->put('/{id}', PlanoController::class . ':update'); //
+            $app->delete('/{id}', PlanoController::class . ':delete'); //
+            $app->delete('/blocoexercicio/{bloco}/{exercicio}', PlanoController::class . ':deleteExercise'); //
+        })->add(AuthMiddleware::class . ':isTreinador');
 
 
-        $app->get('/tipoexercicio/', ExercicioController::class . ':indexTipoExercicio');
-        $app->get('/tipoexercicio/{id}', ExercicioController::class . ':showTipoExercicio');
-        $app->post('/tipoexercicio', ExercicioController::class . ':createTipoExercicio');
-        $app->put('/tipoexercicio/{id}', ExercicioController::class . ':updateTipoExercicio');
-        $app->delete('/tipoexercicio/{id}',  ExercicioController::class . ':deleteTipoExercicio');
-        
-    })->add(AuthMiddleware::class . ':isTreinador');
-});
+        //admins
+        $app->group('', function () use ($app) {
+            $app->get('/', PlanoController::class . ':index'); //
+
+        })->add(AuthMiddleware::class . ':isAdmin');
+    });
+
+    //exercicio
+    $app->group('/exercicio', function () use ($app) {
+
+        //treinador
+        $app->group('', function () use ($app) {
+            $app->get('/', ExercicioController::class . ':index');
+            $app->get('/{id}', ExercicioController::class . ':show');
+            $app->post('/', ExercicioController::class . ':create');
+            $app->put('/{id}', ExercicioController::class . ':update');
+            $app->delete('/{id}',  ExercicioController::class . ':delete');
 
 
+            $app->get('/tipoexercicio/', ExercicioController::class . ':indexTipoExercicio');
+            $app->get('/tipoexercicio/{id}', ExercicioController::class . ':showTipoExercicio');
+            $app->post('/tipoexercicio', ExercicioController::class . ':createTipoExercicio');
+            $app->put('/tipoexercicio/{id}', ExercicioController::class . ':updateTipoExercicio');
+            $app->delete('/tipoexercicio/{id}',  ExercicioController::class . ':deleteTipoExercicio');
+        })->add(AuthMiddleware::class . ':isTreinador');
+    });
+
+    $app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function($req, $res) {
+        $handler = $this->notFoundHandler; // handle using the default Slim page not found handler
+        return $handler($req, $res);
+    });
 
 $app->get('/', function ($request) {
     return "API projeto backend";
